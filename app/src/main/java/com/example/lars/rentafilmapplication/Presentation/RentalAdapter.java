7c74d19ccb837a76fedb3ade6cd781a06a88aabe
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2017-06-19 22:45:56 +0200

    RentalAdapter afgemaakt.

--- app/src/main/java/com/example/lars/rentafilmapplication/Presentation/RentalAdapter.java
+++ app/src/main/java/com/example/lars/rentafilmapplication/Presentation/RentalAdapter.java
@@ -1,10 +1,11 @@
 package com.example.lars.rentafilmapplication.Presentation;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.example.lars.rentafilmapplication.Domain.Rental;
@@ -17,30 +18,67 @@ import java.util.ArrayList;
  */
 
 
-public class RentalAdapter extends ArrayAdapter<Rental> {
+public class RentalAdapter extends BaseAdapter {
     private final String TAG = this.getClass().getSimpleName();
-    private ArrayList filmList;
+    private Context context;
+    private LayoutInflater inflater;
+    private ArrayList rentalList;
 
-    public RentalAdapter(Context context, ArrayList<Rental> rentalList) {
-        super(context, R.layout.custom_listitem_rental, rentalList);
+    public RentalAdapter(Context context, LayoutInflater inflater, ArrayList<Rental> rentalList) {
+        this.context = context;
+        this.inflater = inflater;
+        this.rentalList = rentalList;
     }
 
-    @Override
+    public int getCount() {
+        int size = rentalList.size();
+        return size;
+    }
+
+    public Object getItem(int position) {
+        Log.i(TAG, "getItem: " + position);
+        return rentalList.get(position);
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+
     public View getView(int position, View convertView, ViewGroup parent) {
+        Log.i(TAG, "getView: " + position);
 
-        LayoutInflater productInflater = LayoutInflater.from(getContext());
-        View customView = productInflater.inflate(R.layout.custom_listitem_rental, parent, false);
+        ViewHolder viewHolder;
 
-        Rental singleRental = getItem(position);
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.custom_listitem_rental, null);
 
-        TextView rentalIdTextView = (TextView) customView.findViewById(R.id.rentalIdTextView);
-        TextView customerIdTextView = (TextView) customView.findViewById(R.id.customerIdTextView);
-        TextView inventoryIdTextView = (TextView) customView.findViewById(R.id.inventoryIdTextView);
+            viewHolder = new ViewHolder();
 
-        rentalIdTextView.setText(singleRental.getRentalId());
-        customerIdTextView.setText(singleRental.getCustomerId());
-        inventoryIdTextView.setText(singleRental.getInventoryId());
+            viewHolder.rentalId = (TextView) convertView.findViewById(R.id.rentalIdTextView);
+        TextView customerIdTextView = (TextView) convertView.findViewById(R.id.customerIdTextView);
+        TextView inventoryIdTextView = (TextView) convertView.findViewById(R.id.inventoryIdTextView);
 
-        return customView;
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        Rental singleRental = (Rental) rentalList.get(position);
+        
+        viewHolder.rentalId.setText(singleRental.getRentalId());
+        viewHolder.customerId.setText(singleRental.getCustomerId());
+        viewHolder.inventoryId.setText(singleRental.getInventoryId());
+
+        return convertView;
     }
+
+    private static class ViewHolder {
+        public TextView rentalId;
+        public TextView customerId;
+        public TextView inventoryId;
+
+    }
+
 }
