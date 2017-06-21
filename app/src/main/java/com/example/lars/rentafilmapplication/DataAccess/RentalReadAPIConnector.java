fab3d8981ca5b1e8e05c2c13bb3762d7118c05c2
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2017-06-21 13:17:25 +0200

    Push with RentalAPI, activity.

--- app/src/main/java/com/example/lars/rentafilmapplication/DataAccess/RentalReadAPIConnector.java
+++ app/src/main/java/com/example/lars/rentafilmapplication/DataAccess/RentalReadAPIConnector.java
@@ -1,10 +1,165 @@
 package com.example.lars.rentafilmapplication.DataAccess;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.lars.rentafilmapplication.Domain.Film;
+import com.example.lars.rentafilmapplication.Domain.Rental;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by Kayvon Rahimi on 19-6-2017.
  */
 
-public class RentalReadAPIConnector  {
+public class RentalReadAPIConnector extends AsyncTask<String, Void, String> {
+    private final String TAG = getClass().getSimpleName();
+    private RentalReadAPIConnector.OnRentalAvailable listener = null;
+    private Context context;
+    private ProgressDialog progressBar;
+
+    public RentalReadAPIConnector(OnRentalAvailable listener, Context context) {
+        this.listener = listener;
+        this.context = context;
+    }
+
+    protected void onPreExecute(){
+
+        progressBar = new ProgressDialog(context);
+        progressBar.setCancelable(false);
+        progressBar.setMessage("Fetching rentals");
+        progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressBar.setIndeterminate(true);
+        progressBar.show();
+    }
+
+    protected String doInBackground(String... params) {
+        //
+        InputStream inputStream = null;
+        int responseCode = -1;
+
+        String movieUrl = params[0];
+        String response = "";
+
+        Log.i(TAG, "doInBackground - " + movieUrl);
+
+        //Try-block to establish connection
+        try {
+            // Create productURL object
+            URL url = new URL(movieUrl);
+            // Open connection
+            URLConnection urlConnection = url.openConnection();
+
+            if (!(urlConnection instanceof HttpURLConnection)){
+                return null;
+            }
+
+            HttpURLConnection httpConnection = (HttpURLConnection) urlConnection;
+            httpConnection.setAllowUserInteraction(false);
+            httpConnection.setInstanceFollowRedirects(true);
+            httpConnection.setRequestMethod("GET");
+
+            httpConnection.connect();
+
+            responseCode = httpConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK){
+                inputStream = httpConnection.getInputStream();
+                response = getStringFromInputStream(inputStream);
+            } else {
+                Log.e(TAG, "Error, invalid response");
+            }
+
+            //Exception catching and logging
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "doInBackground MalformedURL " + e.getLocalizedMessage());
+            return null;
+        } catch (IOException e) {
+            Log.e(TAG, "doInBackground IOException " + e.getLocalizedMessage());
+            return null;
+        }catch (Exception e) {
+            Log.e(TAG, "doInBackground Exception " + e.getLocalizedMessage());
+            return null;
+        }
+        return response;
+    }
+
+    //Handle doInBackground method results, retrieve from json, fit form of rental class
+    protected void onPostExecute(String response) {
+
+        if (response == null || response == ""){
+            Log.e(TAG, "onPostExecute got empty response");
+        } else {
+
+            JSONObject jsonObject;
+            try {
+                jsonObject = new JSONObject(response);
+
+                JSONArray items = jsonObject.getJSONArray("items");
+                for (int idx = 0; idx < items.length(); idx++) {
+                    JSONObject rental = items.getJSONObject(idx);
+
+                    int rentalId = rental.getInt("rental_id");
+                    String rentalDate = rental.getString("rental_date");
+                    int inventoryId = rental.getInt("inventory_id");
+                    int customerId = rental.getInt("customer_id");
+                    String returnDate = rental.getString("return_date");
+                    int staffId = rental.getInt("staff_id");
+                    String lastUpdate = rental.getString("last_update");
+
+                    Rental r = new Rental(rentalId, rentalDate, inventoryId, customerId, returnDate, staffId, lastUpdate);
+
+                    listener.onRentalAvailable(r);
+                }
+
+                progressBar.dismiss();
+            } catch (JSONException ex) {
+                Log.e(TAG, "onPostExecute JSONException " + ex.getLocalizedMessage());
+            }
+        }
+    }
+
+
+    private static String getStringFromInputStream(InputStream is) {
+
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    // Call back interface
+    public interface OnRentalAvailable {
+        void onRentalAvailable(Rental rental);
+    }
 }
--- app/src/main/java/com/example/lars/rentafilmapplication/Presentation/RentalActivity.java
+++ app/src/main/java/com/example/lars/rentafilmapplication/Presentation/RentalActivity.java
@@ -1,15 +1,102 @@
 package com.example.lars.rentafilmapplication.Presentation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.example.lars.rentafilmapplication.DataAccess.RentalReadAPIConnector;
+import com.example.lars.rentafilmapplication.Domain.Rental;
 import com.example.lars.rentafilmapplication.R;
 
-public class RentalActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import static com.example.lars.rentafilmapplication.Service.Config.URL_GETRENTALS;
+
+public class RentalActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, RentalReadAPIConnector.OnRentalAvailable{
+    private final String TAG = this.getClass().getSimpleName();
+    private ArrayList<Rental> rentals = new ArrayList<>();
+    private ListView rentalListView;
+    private RentalAdapter rentalAdapter;
+    private EditText searchFld;
+    private RentalReadAPIConnector getRentals;
+    private Button searchBtn;
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rental);
+        toolbar = (Toolbar) findViewById(R.id.toolbar_home);
+        toolbar.setTitle(R.string.home_toolbar);
+        setSupportActionBar(toolbar);
+
+        getRentals = new RentalReadAPIConnector(this, this);
+        getRentals.execute(URL_GETRENTALS);
+
+        rentalListView = (ListView) findViewById(R.id.rentalListView);
+        rentalListView.setOnItemClickListener(this);
+
+        rentalAdapter = new RentalAdapter(getApplicationContext(), getLayoutInflater(), rentals);
+        rentalListView.setAdapter(rentalAdapter);
+
+        //Search textfield + button
+        searchFld = (EditText) findViewById(R.id.searchInput);
+        searchBtn = (Button) findViewById(R.id.searchButton);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.rent_a_film_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.log_out:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            case R.id.home:
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //Select film
+        Rental selectedRental = (Rental) rentals.get(position);
+
+        //Verstuur de informatie van de geklikte rental listitem mee om de rentaldetailactivity mee in te vullen
+        Intent i = new Intent(getApplicationContext(), RentalDetailActivity.class);
+        i.putExtra("Rental", selectedRental);
+        startActivity(i);
+    }
+
+    public void getRental() {
+        RentalReadAPIConnector task = new RentalReadAPIConnector(this, this);
+        String[] urls = new String[]{ URL_GETRENTALS};
+        task.execute(urls);
+    }
+
+    @Override
+    public void onRentalAvailable(Rental rental) {
+        rentals.add(rental);
+        Log.i(TAG, "Rental fetched (" + rental.toString() + ")");
+        rentalAdapter.notifyDataSetChanged();
+    }
+
 }
